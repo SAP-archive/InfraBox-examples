6eabb4af77bc7bd92113327a24444ebd86c32962
Steffen Geissinger <steffen.geissinger@gmail.com>
2017-06-08 10:01:51 +0200

    wait for webserver to be ready

--- compose/infrabox/test/entrypoint.sh
+++ compose/infrabox/test/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
 
-# we wait for the web server to start up
-# Better approach would be to wait until it serves data
-sleep 5 
+# Wait until the webserver is up and running
+until $(curl --output /dev/null --silent --head --fail http://server:3000); do
+    sleep 1
+done
 
 echo "## Run Tests"
 cd /project/tests
